506cbfc1d3717d3318c80d34d24b8e324a5182e5
isimsek7 <isimsekmusic@gmail.com>
2024-09-04 02:29:15 +0300

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace linqGroupJoin
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int ClassId { get; set; }
+    }
+
+    public class Sinif
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using linqGroupJoin;
+
+List<Sinif> classes = new List<Sinif>
+        {
+            new Sinif { ClassId = 1, ClassName = "Matematik" },
+            new Sinif { ClassId = 2, ClassName = "Türkçe" },
+            new Sinif { ClassId = 3, ClassName = "Kimya" }
+        };
+
+List<Student> students = new List<Student>
+        {
+            new Student { StudentId = 1, StudentName = "Ali", ClassId = 1 },
+            new Student { StudentId = 2, StudentName = "Ayşe", ClassId = 2 },
+            new Student { StudentId = 3, StudentName = "Mehmet", ClassId = 1 },
+            new Student { StudentId = 4, StudentName = "Fatma", ClassId = 3 },
+            new Student { StudentId = 5, StudentName = "Ahmet", ClassId = 2 }
+        };
+
+var classGroups = from c in classes
+                  join s in students on c.ClassId equals s.ClassId into studentGroup
+                  select new
+                  {
+                      ClassName = c.ClassName,
+                      Students = studentGroup
+                  };
+foreach (var classGroup in classGroups)
+{
+    Console.WriteLine($"{classGroup.ClassName} sinifi;");
+    foreach (var student in classGroup.Students)
+    {
+        Console.WriteLine($" - {student.StudentName}");
+    }
+}
